40353f9967c28e30819e8c73fc055521988616fb
wyqzl <wuyuqian19950217@qq.com>
2015-12-09 18:28:04 +0800

    顺序输出
    先输入一个整数，确定输入了几个数，动态分配存储空间

--- collator/main.cpp
+++ collator/main.cpp
@@ -7,9 +7,31 @@
 //
 
 #include <iostream>
+//#define N 5
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    int *a;
+    int i,j,temp;
+    int N;
+    scanf("%d",&N);
+    a=(int *)malloc(N*4);
+    for(i=0;i<N;i++)
+        scanf("%d",&a[i]);
+    for(i=0;i<N-1;i++)
+    {
+        for(j=0;j<N-1-i;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+        }
+    }
+    for(i=0;i<N;i++)
+        printf("%d\n",a[i]);
+    
     return 0;
 }
